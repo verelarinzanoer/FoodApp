3da4a7727a925e8609b91616c5d0286ca76bcdf3
verelarinzanoer <ahmadverelzanoe@gmail.com>
2021-03-26 13:34:34 +0700

    Initial Commit

---
+++ FoodApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodApp
+//
+//  Created by MACBOOK on 26/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
